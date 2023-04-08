bcce1cca30f9f9d434f226b4e0816fc9cbb01720
JanSkrabala <jskrabala@notitiamachina.net>
2023-04-08 14:51:53 +0100

    Update sso_config.py

--- sso_config.py
+++ sso_config.py
@@ -1,7 +1,7 @@
 # Azure enterprise app settings
 CLIENT_ID = "Application (client) ID of app registration"
 CLIENT_SECRET = "CLIENT_SECRET"
-AUTHORITY = "https://login.microsoftonline.com/skrabalajanyahoo.onmicrosoft.com"
+AUTHORITY = "https://login.microsoftonline.com/"
 REDIRECT_PATH = "/your redirect URI"
 ENDPOINT = 'https://graph.microsoft.com/v1.0/users'
 SCOPE = ["User.ReadBasic.All"]
